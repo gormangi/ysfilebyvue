6530a6bbc6e514ad1a1f2f5407f3fcb71ed4fffd
gormangi <gormangi@naver.com>
2023-03-29 02:29:33 +0900

    study 5

---
+++ pages/practice/SlotModalLayout.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="model-container">
+  <header>
+    <slot name="header"></slot>
+  </header>
+  <main>
+    <slot></slot>
+  </main>
+  <footer>
+    <slot name="footer"></slot>
+  </footer>
+</div>
+</template>
+
+<script>
+export default {
+  name: "SlotModalLayout",
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/practice/SlotUseModalLayout.vue
@@ -0,0 +1,27 @@
+<template>
+<modal-layout>
+  <template #header>
+    <h1>팝업 타이틀</h1>
+  </template>
+  <template #default>
+    <p>팝업 컨텐츠 1</p>
+    <p>팝업 컨텐츠 2</p>
+  </template>
+  <template #footer>
+    <button type="button">닫기</button>
+  </template>
+</modal-layout>
+</template>
+
+<script>
+import SlotModalLayout from "@/pages/practice/SlotModalLayout";
+
+export default {
+  name: "SlotUseModalLayout",
+  components: {'modal-layout': SlotModalLayout},
+}
+</script>
+
+<style scoped>
+
+</style>
